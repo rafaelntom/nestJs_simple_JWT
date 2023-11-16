227ac12c30051ab4159060a9cc80db950a346e78
RafaelTomazini <rafaelnt83@gmail.com>
2023-11-16 13:08:29 -0300

    feat(auth): login, strategy and service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,9 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
+  validateUser(email: string, password: string) {
+    throw new Error('Method not implemented.');
+  }
   constructor(private readonly prisma: PrismaService) {}
 
   async login() {
